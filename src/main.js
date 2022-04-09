f85a6c8897bb660379c1c13543af792016ee10c6
Santiago Ponce <saponce@ucsc.edu>
2022-04-08 17:40:49 -0700

    Up to 'setting up version control'

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("hellp world");
+let config = { //This is a JS object - similar to a dictionary
+      type: Phaser.CANVAS,
+      width: 640,
+      height: 480
+}
+
+let game = new Phaser.Game(config);
